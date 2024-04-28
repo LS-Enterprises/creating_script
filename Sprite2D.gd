d37c7adaf31744210c472276fc2c55d25f9a3d5d
live-laugh-luke <luke.steel.94@gmail.com>
2024-04-28 18:25:14 +0200

    ic

---
+++ Sprite2D.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+var speed = 800
+var rotation_speed= 2*PI
+var angular_speed= PI
+var velocity_rotation=0
+
+
+func _process(delta):
+	rotation +=  rotation_speed * delta
+	velocity_rotation +=  angular_speed * delta
+	var velocity = Vector2.UP.rotated(velocity_rotation) * speed
+	position += velocity * delta
